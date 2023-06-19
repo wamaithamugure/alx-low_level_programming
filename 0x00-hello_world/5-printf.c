6c0376888e10ec7735bc6778cc7352eb7e839e7d
wamaithamugure <mugurevionna@gmail.com>
2023-06-19 13:59:52 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-include < stdio.h >
-
+#include<stdio.h>
 /**
- * main - A program that prints a line using printf function
- * Return: 0 (success)
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
