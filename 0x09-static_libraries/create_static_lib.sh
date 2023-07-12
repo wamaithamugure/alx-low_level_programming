fd30bec13fc01c4a91046f48117e48e582675ca8
wamaithamugure <mugurevionna@gmail.com>
2023-07-12 12:52:04 -0700

    libraries

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,18 +1,5 @@
-0-isupper.c
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
 
-#include "main.h"
-
-/**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
- */
-
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
